b48f751e232324a1c835f1604556a477bb43aceb
ruster-mw <mateuszwypior2000@gmail.com>
2026-04-29 19:05:37 +0200

    polished player physics

--- Scripts/playerMovement.gd
+++ Scripts/playerMovement.gd
@@ -7,45 +7,40 @@ extends RigidBody2D
 @onready var floorRayCast : RayCast2D = $FloorRayCast
 @onready var leftFloorRayCast : RayCast2D = $LeftFloorRayCast
 @onready var rightFloorRayCast : RayCast2D = $RightFloorRayCast
+@onready var shapeCase : ShapeCast2D  = $FloorShapeCast
+
 
 var facingRight : bool = true
 var getrotation : float = 0.0
 var maxUpRightAngle : float = 7.2
 var maxTurnAngle : float = 53.5
 var balancePower : float = 2000.0
-var turnSpeed : float = 280
+var maxTurnSpeed : float = 280
+var jumpPower : float = 1100.0
+var lerpTurnTime : float = 0.5
+var turningTime : float
+var unbalanacedTime : float
 
 var inContact : bool = false
 var isTurning : bool
 var turnRight : bool
 var hasAppliedDamp : bool
 
-func _balance() -> void:
+
+func _balance(delta : float) -> void:
 	getrotation = rotation_degrees
 	getrotation = wrapf(getrotation, -180.0, 180.0)
 	if (absf(getrotation) > maxUpRightAngle):
 		if (inContact):
-			var directionPower = -getrotation
-			apply_torque(directionPower * balancePower)
-			#hasAppliedDamp = false
+			unbalanacedTime += delta
+			var extraForceMult = clamp(ceil(unbalanacedTime / 1.2), 1, 4)
+			var directionPower = clamp(-getrotation, -90.0, 90.0)
+			apply_torque(directionPower * extraForceMult * balancePower)
 	else:
-		if hasAppliedDamp == false:
+		if unbalanacedTime > 0:
 			angular_velocity = angular_velocity / 100.0
-			hasAppliedDamp = true
-
-
-
-func _handleVisuals() -> void:
-	sprite.flip_h = not facingRight
-
-#func _handleInput() -> void:
-	#if Input.is_action_just_pressed('move_left'):
-		#_jump(-velocityX)
-	#elif Input.is_action_just_pressed('move_right'):	
-		#_jump(velocityX)
-#
-#func _jump(horizontal: float) -> void:
-	#facingRight = horizontal > 0
+			unbalanacedTime = 0
+			
 
 func _startTurn(turningRight : bool) -> void:
 	if (isTurning || isGrounded() == false):
@@ -55,18 +50,31 @@ func _startTurn(turningRight : bool) -> void:
 	isTurning = true
 
 func _turn(delta : float) -> void:
+	turningTime += delta
+	
 	getrotation = rotation_degrees
 	getrotation = wrapf(getrotation, -180.0, 180.0)
 	var direction = 1 if turnRight else -1
 	
+	var turnSpeed : float = min(turningTime / lerpTurnTime, 1.0) * maxTurnSpeed
 	if ((getrotation > maxTurnAngle * direction) == !turnRight):
-		print(getrotation + turnSpeed * direction * delta)
 		rotation_degrees = getrotation + turnSpeed * direction * delta
 		
 	
 func isGrounded() -> bool:
 	return floorRayCast.is_colliding() || rightFloorRayCast.is_colliding() || leftFloorRayCast.is_colliding()
 
+func jump(jumpRight : bool) -> void:
+	if (!isTurning || jumpRight != turnRight):
+		return;
+	if (isGrounded()):
+		apply_central_impulse(Vector2.UP.rotated(rotation) * jumpPower)
+	isTurning = false
+	lock_rotation = false
+	turningTime = 0
+
+func _handleVisuals() -> void:
+	sprite.flip_h = not facingRight
 
 
 
@@ -77,16 +85,32 @@ func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("move_left"):
+	if Input.is_action_pressed("move_left"):
 		_startTurn(false)
-	elif Input.is_action_just_pressed("move_right"):
+	elif Input.is_action_pressed("move_right"):
 		_startTurn(true)
+		
+	if Input.is_action_just_released("jump_left"):
+		jump(false)
+	elif Input.is_action_just_released("jump_right"):
+		jump(true)
 
 func _physics_process(delta: float) -> void:
 	inContact = get_contact_count() > 0
 	if (isTurning):
 		_turn(delta)
 	else: 
-		_balance()
+		_balance(delta)
 	#_handleInput()
 	_handleVisuals()
+	
+	
+	
+	#func _handleInput() -> void:
+	#if Input.is_action_just_pressed('move_left'):
+		#_jump(-velocityX)
+	#elif Input.is_action_just_pressed('move_right'):	
+		#_jump(velocityX)
+#
+#func _jump(horizontal: float) -> void:
+	#facingRight = horizontal > 0
